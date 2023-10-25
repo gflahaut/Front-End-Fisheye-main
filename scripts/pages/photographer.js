f577d0db7d8b084eaf0c5f81b9584500ed81f3fa
flahaut <flahaut.gwendoline@gmail.com>
2023-10-25 13:49:14 +0200

    add comments + JsDoc + display necessary infos

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,31 +1,134 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// Importez la fonction getOnePhotographer du module "request.js"
 import { getOnePhotographer } from "../utils/request.js";
 
+// Analysez les paramètres de l'URL pour obtenir l'ID du photographe
 const params = new URLSearchParams(window.location.search);
-const photographerId = params.get('id');
-const onePhotographer = await getOnePhotographer(photographerId);
-console.log(onePhotographer);
-onePhotographer.media = onePhotographer.media.filter(media => media.photographerId == photographerId);
-onePhotographer.photographers = onePhotographer.photographers.filter(photographer => photographer.id == photographerId);
-console.log(onePhotographer);
+const photographerId = params.get("id");
 
+// Appelez la fonction getOnePhotographer pour obtenir les détails du photographe
+const onePhotographer = await getOnePhotographer(photographerId);
+
+// Filtrez les médias pour ne conserver que ceux du photographe actuel
+onePhotographer.media = onePhotographer.media.filter(
+    (media) => media.photographerId == photographerId
+);
+
+// Filtrez les photographes pour ne conserver que le photographe actuel
+onePhotographer.photographers = onePhotographer.photographers.filter(
+    (photographer) => photographer.id == photographerId
+);
+
+// Récupérez les détails du photographe
 const photographer = onePhotographer.photographers[0];
+
+// Sélectionnez l'élément où afficher les informations du photographe
 const photographerInfo = document.querySelector(".photographer-nav-info");
+
+// Affichez les informations du photographe dans le HTML
 photographerInfo.innerHTML = `
     <nav>
     <ul>
-    <li><h2>${photographer.name}</h2></li>
+    <li><h1>${photographer.name}</h1></li>
     <li><p>${photographer.city}, ${photographer.country}</p></li>
     <li><p>${photographer.tagline}</p></li>
     <ul>
     </nav>
     <figure>
-    <img src="assets/photographers/${photographer.portrait}" alt="photo de ${photographer.name}" class="round-image"></img>
+    <img src="assets/photographers/${photographer.portrait}" alt="${photographer.name}" class="round-image"></img>
     </figure>
 `;
-// const mediaContainer = document.getElementsByClassName("media-container");
 
-// photographerMedia.forEach(mediaData => {
-//     const media = PhotographerFactory.createMedia(mediaData);
-//     mediaContainer.innerHTML += generateMediaHTML(media);
-// });
+// Appelez la fonction displayMedia pour afficher les médias
+displayMedia(onePhotographer.media);
+
+// Sélectionnez l'élément de sélection pour trier les médias
+const sortSelect = document.getElementById("sort-select");
+
+// Ajoutez un gestionnaire d'événements pour réagir aux changements de sélection
+sortSelect.addEventListener("change", function () {
+    const selectedOption = sortSelect.value;
+    /**
+     * Fonction de gestion du changement de tri des médias par popularité.
+     * @param {string} selectedOption - L'option de tri sélectionnée ("popularite" dans ce cas).
+     */
+    if (selectedOption === "popularite") {
+        // Trier les médias par popularité (du plus aimé au moins aimé)
+        onePhotographer.media.sort((a, b) => b.likes - a.likes);
+
+        // Appeler la fonction displayMedia pour afficher les médias triés
+        displayMedia(onePhotographer.media);
+    } else if (selectedOption === "date") {
+        // Trier par date (du plus récent au moins récent)
+        /**
+         * Cette méthode trie un tableau de médias en fonction de la date.
+         * @param {Array} media - Le tableau de médias à trier.
+         */
+        onePhotographer.media.sort((a, b) => {
+            // Convertir les dates en objets Date pour une comparaison précise
+            const dateA = new Date(a.date);
+            const dateB = new Date(b.date);
+
+            // Comparer les dates
+            if (dateA < dateB) {
+                // Si la date de 'a' est inférieure à la date de 'b', renvoyer 1
+                return 1;
+            }
+            if (dateA > dateB) {
+                // Si la date de 'a' est supérieure à la date de 'b', renvoyer -1
+                return -1;
+            }
+
+            // Si les dates sont égales, renvoyer 0
+            return 0;
+        });
+
+        displayMedia(onePhotographer.media);
+        /**
+         * Fonction de gestion du changement de tri des médias par titre.
+         * @param {string} selectedOption - L'option de tri sélectionnée ("titre" dans ce cas).
+         */
+    } else if (selectedOption === "titre") {
+        // Trier les médias par titre (ordre alphabétique)
+        onePhotographer.media.sort((a, b) => {
+            // Convertir les titres en minuscules pour une comparaison insensible à la casse
+            const titleA = a.title.toLowerCase();
+            const titleB = b.title.toLowerCase();
+
+            if (titleA < titleB) {
+                return -1; // Si le titre de 'a' précède le titre de 'b', renvoyer -1
+            }
+            if (titleA > titleB) {
+                return 1; // Si le titre de 'b' précède le titre de 'a', renvoyer 1
+            }
+            return 0; // Si les titres sont égaux, renvoyer 0
+        });
+
+        // Appeler la fonction displayMedia pour afficher les médias triés
+        displayMedia(onePhotographer.media);
+    }
+});
+
+/**
+ * Affiche les médias dans le DOM en utilisant des modèles.
+ * @param {Array} medias - Le tableau de médias à afficher.
+ */
+function displayMedia(medias) {
+    // Sélectionnez l'élément du DOM où afficher les médias
+    const mediaContainer = document.querySelector(".media-container");
+    
+    // Réinitialisez le contenu de l'élément pour supprimer les médias précédents
+    mediaContainer.innerHTML = "";
+    
+    // Parcourez tous les médias et ajoutez-les au conteneur
+    medias.forEach((media) => {
+        // Créez un modèle de média en utilisant la fonction mediaTemplate
+        const mediaModel = mediaTemplate(media);
+        
+        // Obtenez le DOM du média en utilisant la méthode getMediaCardDOM du modèle
+        const mediaCardDOM = mediaModel.getMediaCardDOM();
+        
+        // Ajoutez le DOM du média au conteneur
+        mediaContainer.appendChild(mediaCardDOM);
+    });
+}
+
--- scripts/templates/media.js
+++ scripts/templates/media.js
@@ -1,48 +1,70 @@
+/**
+ * Fonction qui génère un modèle de média à partir des données du média.
+ * @param {Object} mediaData - Les données du média.
+ * @returns {Object} - Un objet contenant des propriétés et une méthode pour générer le DOM du média.
+ */
 function mediaTemplate(mediaData) {
-    const { title, image, video, likes, date, price} = mediaData;
-
+    const { title, image, video, likes, date, price } = mediaData;
     let mediaImage = "";
     let mediaVideo = "";
-    if (image.length > 0){
+
+    // Vérifiez si l'image existe et n'est pas nulle
+    if (typeof image !== undefined && image) {
         mediaImage = `assets/images/${mediaData.photographerId}/${image}`;
-    }else{
+    }
+    if (typeof video !== undefined && video) {
         mediaVideo = `assets/images/${mediaData.photographerId}/${video}`;
     }
-    
+
+    /**
+     * Méthode pour générer le DOM du média (article HTML).
+     * @returns {HTMLElement} - Élément HTML représentant le média.
+     */
     function getMediaCardDOM() {
+        const article = document.createElement('article');
         const figure = document.createElement('figure');
-        img.setAttribute('src', );
-        img.setAttribute('alt', `Photo de ${name}`);
-        img.classList.add('round-image'); // Ajoutez la classe "round-image" à l'image
+
+        // Fonction pour générer le contenu HTML du média en fonction du type (image ou vidéo)
+        function generateMediaHTML(mediaImage, mediaVideo) {
+            if (mediaImage.length > 0) {
+                const img = document.createElement('img');
+                img.setAttribute('src', mediaImage);
+                img.setAttribute('alt', `${title}`);
+                img.classList.add('media-image');
+                figure.appendChild(img);
+            }
+            if (mediaVideo.length > 0) {
+                const video = document.createElement('video');
+                const meta = document.createElement('meta');
+                const type = "video/mp4";
+                const videoName = title;
+                video.setAttribute('controls', '');
+                video.setAttribute('src', mediaVideo);
+                video.setAttribute('type', type);
+                video.classList.add('media-video');
+                meta.setAttribute('itemprop', 'name');
+                meta.setAttribute('content', videoName);
+                figure.appendChild(video);
+                video.appendChild(meta);
+            }
+        }
+
+        // Appel de la fonction pour générer le contenu HTML du média
+        generateMediaHTML(mediaImage, mediaVideo);
+
         const figcaption = document.createElement('figcaption');
         const h2 = document.createElement('h2');
-        h2.textContent = name;
-        const h3 = document.createElement('h3');
-        h3.textContent = `${city}, ${country}`;
-        const h4 = document.createElement('h4');
-        h4.textContent = `${tagline}`;
-        const h5 = document.createElement('h5');
-        h5.textContent = `${price}€`;
-        figure.addEventListener('click', function() {
-            const photographerUrl = `/photographer.html?id=${id}`;
-            window.location.href = photographerUrl;
-        });
-        figure.appendChild(img);
-        figcaption.appendChild(h2); 
-        figcaption.appendChild(h3); 
-        figcaption.appendChild(h4); 
-        figcaption.appendChild(h5);
+        h2.textContent = title;
+        figcaption.appendChild(h2);
+        const likesInfo = document.createElement('span');
+        figcaption.appendChild(likesInfo);
+        likesInfo.textContent = likes;
         figure.appendChild(figcaption);
         article.appendChild(figure);
+
         return article;
     }
-    return { name, id, city, country, tagline, price, picture, getUserCardDOM }
-}
 
-// function generateMediaHTML(media) {
-// if (media instanceof ImageMedia) {
-// return `<img src="${media.image}" alt="${media.title}">`;
-// } else if (media instanceof VideoMedia) {
-// return `<video controls><source src="${media.video}" type="video/mp4">Your browser does not support the video tag.</video>`;
-// }
-// }
+    // Retourne un objet contenant des propriétés et la méthode de génération du DOM du média
+    return { title, image, video, likes, date, price, getMediaCardDOM };
+}
